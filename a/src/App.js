692d035b49f8e7c895bf15805d4a133b1ef76ae6
loheunice <euniceloh07@gmail.com>
2025-02-10 21:30:13 +0800

    react

--- a/src/App.js
+++ a/src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import { Provider } from "@/components/ui/provider"
 
 function App({ Component, pageProps }) {
   return (
-    <Provider>
-      <Component {...pageProps} />
-    </Provider>
-  )
-}
-
-function App() {
-  return (
-    <div className="app">
-     
-    </div>
+      <div className="app">
+        <Component {...pageProps} />
+      </div>
   );
 }
 
